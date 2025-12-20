622d8f6f509edf6ff893a5eec6e97854c52f3d80
Umaira Yusra <uyusra46@gmail.com>
2025-12-20 15:05:46 +0900

    Django Service Fulfillment

--- PhotoBlogServer/myapp/blog/views.py
+++ PhotoBlogServer/myapp/blog/views.py
@@ -19,7 +19,11 @@ class blogImage(viewsets.ModelViewSet):
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    title_filter=request.GET.get('title')
+    if title_filter:
+        posts = Post.objects.filter(title=title_filter)
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
